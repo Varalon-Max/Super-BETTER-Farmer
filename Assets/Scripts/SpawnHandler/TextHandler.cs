33117e84be304e03c6468c8c4647f3bfdace317c
AndrewMikhaylov <99182259+AndrewMikhaylov@users.noreply.github.com>
2023-03-11 22:26:37 +0200

    added features with instantiating

---
+++ Assets/Scripts/SpawnHandler/TextHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using TurnHandler.TwoPlayers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpawnHandler
+{
+    public class TextHandler: MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _spawnCounters;
+        [SerializeField] private TurnChecker _turnChecker;
+        private TextMeshProUGUI _currentTextPlaceholder;
+        public void IncreaseNumber(int numberOfTier, int newNumber)
+        {
+            if (!_turnChecker._isFirstPlayerTurn)
+            {
+                numberOfTier += 5;
+            }
+            _currentTextPlaceholder = _spawnCounters[numberOfTier].GetComponent<TextMeshProUGUI>();
+            _currentTextPlaceholder.text = newNumber.ToString();
+        }
+    }
+}
